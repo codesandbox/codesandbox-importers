366e62af73bac6cbd688f5eec31c2a659a52beb5
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-06-26 16:31:07 +0200

    Add pm2 configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    // First application
+    {
+      name: 'Git Extractor',
+      script: 'dist/index.js',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'bundler',
+      host: 'ssh.codesandbox.io',
+      ref: 'origin/master',
+      repo: 'git@github.com:CompuIves/codesandbox-git-extractor.git',
+      path: '/home/git-extractor',
+      'post-deploy':
+        'yarn && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
