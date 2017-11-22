001e6e7bd431a4e6e4d4cedbb00a51a6e2688461
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-11-22 19:26:05 +0100

    Support JSON objects for define endpoint

--- src/routes/define.ts
+++ src/routes/define.ts
@@ -5,13 +5,21 @@ import createSandbox from '../utils/sandbox/create-sandbox/index';
 export const define = async (ctx: Context, next: () => Promise<any>) => {
   const { files } = ctx.request.body;
 
-  const normalizedFiles: INormalizedModules = files.reduce(
-    (total: INormalizedModules, next: IModule & { path: string }) => ({
-      ...total,
-      [next.path]: next,
-    }),
-    {}
-  );
+  const normalizedFiles: INormalizedModules = files
+    .map((file: IModule) => {
+      if (typeof file.content === 'object') {
+        return { ...file, content: JSON.stringify(file.content, null, 2) };
+      }
+
+      return file;
+    })
+    .reduce(
+      (total: INormalizedModules, next: IModule & { path: string }) => ({
+        ...total,
+        [next.path]: next,
+      }),
+      {}
+    );
 
   const sandbox = await createSandbox(normalizedFiles);
 
