71b48f71ce315303f2fc26479b754c0a2aca05dc
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2018-02-23 16:42:50 +0100

    Move define to codesandbox-import-utils

--- packages/cli/src/api/define.ts
+++ packages/cli/src/api/define.ts
@@ -1,19 +1,3 @@
-import * as LZString from 'lz-string';
+import { getParameters } from 'codesandbox-import-utils/lib/api/define';
 
-export interface IFiles {
-  [key: string]: {
-    content: string;
-    isBinary: boolean;
-  };
-}
-
-function compress(input: string) {
-  return LZString.compressToBase64(input)
-    .replace(/\+/g, `-`) // Convert '+' to '-'
-    .replace(/\//g, `_`) // Convert '/' to '_'
-    .replace(/=+$/, ``); // Remove ending '='
-}
-
-export function getParameters(files: IFiles) {
-  return compress(JSON.stringify(files));
-}
+export { getParameters };
