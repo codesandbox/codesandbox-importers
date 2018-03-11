84debf20d13da39de88d362302a8c092e1df9f27
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2018-03-11 15:21:57 +0100

    Allow custom template setting

--- packages/import-utils/src/create-sandbox/templates.ts
+++ packages/import-utils/src/create-sandbox/templates.ts
@@ -1,24 +1,24 @@
-import { INormalizedModules } from 'codesandbox-import-util-types';
-import { ITemplate } from 'codesandbox-import-util-types';
+import { INormalizedModules } from "codesandbox-import-util-types";
+import { ITemplate } from "codesandbox-import-util-types";
 
 export function getMainFile(template: ITemplate) {
-  if (template === 'vue-cli') {
-    return 'src/main.js';
+  if (template === "vue-cli") {
+    return "src/main.js";
   }
 
-  if (template === 'angular-cli') {
-    return 'src/main.ts';
+  if (template === "angular-cli") {
+    return "src/main.ts";
   }
 
-  if (template === 'create-react-app-typescript') {
-    return 'src/index.tsx';
+  if (template === "create-react-app-typescript") {
+    return "src/index.tsx";
   }
 
-  if (template === 'parcel') {
-    return 'index.html';
+  if (template === "parcel") {
+    return "index.html";
   }
 
-  return 'src/index.js';
+  return "src/index.js";
 }
 
 export function getTemplate(
@@ -28,40 +28,50 @@ export function getTemplate(
   },
   modules: INormalizedModules
 ): ITemplate {
-  if (Object.keys(modules).find(m => m.endsWith('.vue'))) {
-    return 'vue-cli';
+  if (modules["/sandbox.config.json"]) {
+    try {
+      const config = JSON.parse(modules["/sandbox.config.json"].content);
+
+      if (config.template) {
+        return config.template;
+      }
+    } catch (e) {}
+  }
+
+  if (Object.keys(modules).find(m => m.endsWith(".vue"))) {
+    return "vue-cli";
   }
 
   const { dependencies = {}, devDependencies = {} } = packageJSONPackage;
 
   const totalDependencies = [
     ...Object.keys(dependencies),
-    ...Object.keys(devDependencies),
+    ...Object.keys(devDependencies)
   ];
 
-  if (totalDependencies.indexOf('parcel-bundler') > -1) {
-    return 'parcel';
+  if (totalDependencies.indexOf("parcel-bundler") > -1) {
+    return "parcel";
   }
 
-  if (totalDependencies.indexOf('react-scripts-ts') > -1) {
-    return 'create-react-app-typescript';
+  if (totalDependencies.indexOf("react-scripts-ts") > -1) {
+    return "create-react-app-typescript";
   }
 
-  if (totalDependencies.indexOf('@angular/core') > -1) {
-    return 'angular-cli';
+  if (totalDependencies.indexOf("@angular/core") > -1) {
+    return "angular-cli";
   }
 
-  if (totalDependencies.indexOf('preact-cli') > -1) {
-    return 'preact-cli';
+  if (totalDependencies.indexOf("preact-cli") > -1) {
+    return "preact-cli";
   }
 
-  if (totalDependencies.indexOf('svelte') > -1) {
-    return 'svelte';
+  if (totalDependencies.indexOf("svelte") > -1) {
+    return "svelte";
   }
 
-  if (totalDependencies.indexOf('vue') > -1) {
-    return 'vue-cli';
+  if (totalDependencies.indexOf("vue") > -1) {
+    return "vue-cli";
   }
 
-  return 'create-react-app';
+  return "create-react-app";
 }
