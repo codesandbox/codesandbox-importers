028b90122537803f4614d58bdd6b5ff12a22c71a
Ives van Hoorne <ives.v.h@gmail.com>
2020-04-11 18:44:17 +0200

    Add comment

--- packages/import-utils/src/is-text.ts
+++ packages/import-utils/src/is-text.ts
@@ -3,7 +3,7 @@ const _isText = require("istextorbinary").isText;
 const jsRegex = /(t|j)sx?$/i;
 
 const FILE_LOADER_REGEX = /\.(ico|jpg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm)(\?.*)?$/i;
-export const MAX_FILE_SIZE = 3 * 1024 * 1024;
+export const MAX_FILE_SIZE = 3 * 1024 * 1024; // 3 MB
 
 export const isText = (filename: string, buffer: Buffer) => {
   if (jsRegex.test(filename)) {
