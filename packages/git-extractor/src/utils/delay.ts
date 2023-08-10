61b2fcd9d191a855e72c1390bfa709e25c456e66
Ives van Hoorne <ives@codesandbox.io>
2023-08-10 11:34:43 +0200

    chore(deps): update lerna, jest, typescript (#185)
    * chore(deps): update lerna, jest, typescript
    
    * refactor(cli): update tsconfig compiler options
    
    * fix dependency duplicate

--- packages/git-extractor/src/utils/delay.ts
+++ packages/git-extractor/src/utils/delay.ts
@@ -1,4 +1,4 @@
-export default function delay(ms: number) {
+export default function delay(ms: number): Promise<void> {
   return new Promise((resolve) => {
     setTimeout(() => resolve(), ms);
   });
