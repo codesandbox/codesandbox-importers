1b9d0549abf49a594bb4ca668334acb921aea874
Ives van Hoorne <ives@codesandbox.io>
2024-04-11 13:26:00 +0200

    fix(define): don't camelize files (#204)

--- packages/git-extractor/src/middleware/camelize.ts
+++ packages/git-extractor/src/middleware/camelize.ts
@@ -3,7 +3,14 @@ import { camelizeKeys } from "humps";
 
 const camelizeMiddleware = async (ctx: Context, next: () => Promise<any>) => {
   if (ctx.request.body) {
+    const originalBody = ctx.request.body;
     ctx.request.body = camelizeKeys(ctx.request.body);
+
+    // Don't camelize files object, because there will be paths
+    // with underscores and it's user input.
+    if (ctx.request.body.files) {
+      ctx.request.body.files = originalBody.files;
+    }
   }
 
   await next();
